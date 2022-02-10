82e8d0c9b2583209e9d2d8979f49bf6f55ad2406
manuelg-nimble <manuelg@nimble.la>
2022-02-09 22:04:25 -0300

    water pump basic control

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <SimpleTimer.h>
 #include "./utils/MQTTConnector.h"
 
 //########################################################## CONSTANTS #############################################################
 
-String sensorTopic = "home/office/door";
-String ALL_OK = "ALL_OK";
+String sensorTopic = "home/terrace/pump";
 String ON = "ON";
 String OFF = "OFF";
-String ALARM = "ALARM";
 String DEACTIVATED = "DEACTIVATED";
-
+String REPORT = "REPORT";
 
 //########################################################## WIFI CREDENTIALS #############################################################
 
@@ -22,16 +19,13 @@ char pass[] = "heyholetsgo";
 
 void setupWifi();
 void mqttCallback(char *, byte *, unsigned int);
-void getSensorValue();
 void blink();
 void checkPayload(String);
 
 //########################################################## GLOBALS #############################################################
 
-int sensor = D0; // magnetic or otherwise triggereable sensor
+int pumpPin = D5; // water pump is connected to D5 pin
 bool isActivated = true;
-String currentState = ALL_OK;
-SimpleTimer timer;
 
 //########################################################## CODE #############################################################
 
@@ -43,10 +37,8 @@ void setup()
   // using mac address as device ID for topic
   MQTTBegin();
   MQTTSetCallback(mqttCallback);
-  pinMode(sensor, INPUT);
+  pinMode(pumpPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  timer.setInterval(1000L, getSensorValue);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
@@ -67,38 +59,6 @@ void setupWifi()
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("Device Ready for Operation");
-}
-
-/**
- * @brief Get the Sensor Value from connected sensor
- * and publish to MQTT broker
- */
-void getSensorValue()
-{
-  if (isActivated)
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-    if (digitalRead(sensor) == LOW && !currentState.equals(ALL_OK))
-
-    {
-      //door is closed ALL GOOD
-      currentState = ALL_OK;
-      MQTTPublish(sensorTopic, ALL_OK);
-    }
-    if (digitalRead(sensor) == HIGH && !currentState.equals(ALARM))
-    {
-      blink();
-      //door is open ALARM ALARM
-      currentState = ALARM;
-      MQTTPublish(sensorTopic, ALARM);
-    }
-  } else {
-    if (!currentState.equals(DEACTIVATED)){
-      MQTTPublish(sensorTopic, DEACTIVATED);
-      currentState = DEACTIVATED;
-    }
-    
-  }
 }
 
 /**
@@ -131,12 +91,17 @@ void checkPayload(String payload)
     if (payload.equals(ON))
     {
       digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(pumpPin, HIGH);
       isActivated = true;
     }
     else if (payload.equals(OFF))
     {
       isActivated = false;
+      digitalWrite(pumpPin, LOW);
       digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else if (payload.equals(REPORT)) {
+      //TODO: mqtt publish report json
     }
   }
 }
@@ -155,7 +120,6 @@ void blink()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  timer.run();
   MQTTLoop();
   MQTTSubscribe(sensorTopic);
 }
